4a0e1f11b2378fe4b57e5f396df4f00907bd2927
김형익 <kiomnd2@naver.com>
2024-08-07 16:24:31 +0900

    reservation 유저 정보 호출 테스트

--- junkward-app-internal-api/src/main/java/junkyard/reservation/inferfaces/ReservationApi.java
+++ junkward-app-internal-api/src/main/java/junkyard/reservation/inferfaces/ReservationApi.java
@@ -1,9 +1,13 @@
 package junkyard.reservation.inferfaces;
 
+import junkyard.member.domain.MemberUser;
 import junkyard.reservation.application.ReservationFacade;
 import junkyard.response.CommonResponse;
 import junkyard.utils.IdempotencyKeyCreator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,15 +21,17 @@ public class ReservationApi {
     private final ReservationFacade reservationFacade;
 
     @PostMapping("/checkout")
-    public CommonResponse<ReservationDto.ResponseCheckout> checkoutReservation(@RequestBody ReservationDto.RequestCheckout checkout) {
+    public CommonResponse<ReservationDto.ResponseReservationCheckout> checkoutReservation(@AuthenticationPrincipal MemberUser memberUser) {
         return CommonResponse.success(
-                ReservationDto.ResponseCheckout.builder()
-                        .idempotencyKey(IdempotencyKeyCreator.create(checkout.getSeed()))
+                ReservationDto.ResponseReservationCheckout.builder()
+                        .idempotencyKey(IdempotencyKeyCreator.create(memberUser.getAuthId()))
                         .build()
         );
     }
 
-    public CommonResponse<ReservationDto.ResponseReservation> reservation(@RequestBody ReservationDto.RequestReservation requestReservation) {
+    public CommonResponse<ReservationDto.ResponseReservation> reservation(@AuthenticationPrincipal MemberUser memberUser,
+                                                                          @RequestBody ReservationDto.RequestReservation requestReservation) {
+
         return CommonResponse.success(ReservationDto.ResponseReservation.builder().build());
     }
 
--- junkward-app-internal-api/src/main/java/junkyard/reservation/inferfaces/ReservationDto.java
+++ junkward-app-internal-api/src/main/java/junkyard/reservation/inferfaces/ReservationDto.java
@@ -5,38 +5,49 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 public class ReservationDto {
 
     @NoArgsConstructor
     @AllArgsConstructor
     @Builder
     @Getter
-    public static class RequestReservation {
-        private String reservationId;
-
+    public static class RequestReservationCheckout {
+        private String seed;
     }
 
     @NoArgsConstructor
     @AllArgsConstructor
+    @Builder
+    @Getter
+    public static class ResponseReservationCheckout {
+        private String idempotencyKey;
+    }
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    @Getter
+    public static class RequestReservation {
+        private String contents;
+        private List<RequestCars> carList;
+    }
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    @Getter
+    public static class RequestCars {
+        private String make;
+        private String model;
+        private String licensePlate;
+    }
+
+    @NoArgsConstructor
     @Builder
     @Getter
     public static class ResponseReservation {
 
-    }
-
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @Getter
-    @Builder
-    public static class RequestCheckout {
-        private String seed;
-    }
-
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @Getter
-    @Builder
-    public static class ResponseCheckout {
-        private String idempotencyKey;
     }
 }
