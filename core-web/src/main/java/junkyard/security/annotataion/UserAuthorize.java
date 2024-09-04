f5d6b3dfdfc55fc8303ddb85dacd7dbc13dfa8b2
kiomnd2 <kiomnd2@gmail.com>
2024-09-04 22:18:00 +0900

    anntation 권한 설정

--- core-web/src/main/java/junkyard/security/annotataion/UserAuthorize.java
+++ core-web/src/main/java/junkyard/security/annotataion/UserAuthorize.java
@@ -9,6 +9,6 @@ import java.lang.annotation.Target;
 
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-@PreAuthorize("hasAuthority('USER')")
+@PreAuthorize("hasRole('ROLE_USER')")
 public @interface UserAuthorize {
 }
--- junkward-app-internal-api/src/main/java/junkyard/JunkyardApiApplication.java
+++ junkward-app-internal-api/src/main/java/junkyard/JunkyardApiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
+
 @PropertySource(encoding = "UTF-8", ignoreResourceNotFound = true,
 value = {
         "classpath:application-junkyard-api-${spring.profiles.active}.properties"
