d2a4c2b5c82842888940fab66d9e113b81bd8965
kiomnd2 <kiomnd2@gmail.com>
2024-09-22 10:42:28 +0900

    모듈 수정

--- junkyard-telegram-server/src/main/java/junkyard/JunkyardCallerApplication.java
+++ junkyard-telegram-server/src/main/java/junkyard/JunkyardCallerApplication.java
@@ -3,9 +3,14 @@ package junkyard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@PropertySource(encoding = "UTF-8", ignoreResourceNotFound = true,
+        value = {
+                "classpath:application-alarm-${spring.profiles.active}.properties"
+        })
 public class JunkyardCallerApplication {
 
     public static void main(String[] args){
