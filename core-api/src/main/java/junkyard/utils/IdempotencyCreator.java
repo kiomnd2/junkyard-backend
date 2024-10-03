b78cd953debe13614f80c27e38ed22593bd86835
kiomnd2 <kiomnd2@gmail.com>
2024-10-03 22:18:57 +0900

    uuid 재설정

--- core-api/src/main/java/junkyard/utils/IdempotencyCreator.java
+++ core-api/src/main/java/junkyard/utils/IdempotencyCreator.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 @UtilityClass
 public class IdempotencyCreator {
 
-    public String create(Object data) {
-        return UUID.nameUUIDFromBytes(data.toString().getBytes()).toString();
+    public String create() {
+        return UUID.randomUUID().toString();
     }
 }
