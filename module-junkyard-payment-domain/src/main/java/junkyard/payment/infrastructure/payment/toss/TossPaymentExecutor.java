324f10c024b01efe32936bc98b190851bd09ffb1
kiomnd2 <kiomnd2@gmail.com>
2025-01-12 10:18:40 +0900

    토스페이먼츠 에러 처리

--- module-junkyard-payment-domain/src/main/java/junkyard/payment/infrastructure/payment/toss/TossPaymentExecutor.java
+++ module-junkyard-payment-domain/src/main/java/junkyard/payment/infrastructure/payment/toss/TossPaymentExecutor.java
@@ -1,12 +1,20 @@
 package junkyard.payment.infrastructure.payment.toss;
 
 import junkyard.payment.domain.*;
+import junkyard.payment.infrastructure.payment.toss.exception.PSPConfirmationException;
+import junkyard.payment.infrastructure.payment.toss.exception.TossPaymentError;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.function.Function;
 
 @RequiredArgsConstructor
 @Component
@@ -31,12 +39,19 @@ public class TossPaymentExecutor implements PaymentExecutor {
                 .header("Idempotency-Key", command.getOrderId())
                 .bodyValue(bodyValue)
                 .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, getClientResponseError())
+                .onStatus(HttpStatusCode::is5xxServerError, getClientResponseError())
                 .bodyToMono(TossPaymentConfirmationResponse.class)
+                .retryWhen(Retry.backoff(2, Duration.ofSeconds(1)).jitter(0.1)
+                        .filter(throwable -> (throwable instanceof PSPConfirmationException &&
+                                ((PSPConfirmationException)throwable).getIsRetryableError()))
+                        .onRetryExhaustedThrow((retryBackoffSpec, retrySignal) ->
+                                retrySignal.failure())
+                )
                 .block();
-        ;
         return PaymentExecutionResult.builder()
-                .paymentKey(response.getPaymentKey())
-                .orderId(response.getOrderId())
+                .paymentKey(command.getPaymentKey())
+                .orderId(command.getOrderId())
                 .paymentExtraDetails(PaymentExecutionResult.PaymentExtraDetails.builder()
                         .paymentType(PaymentEvent.PaymentType.get(response.getType()))
                         .paymentMethod(PaymentEvent.PaymentMethod.get(response.getMethod()))
@@ -51,5 +66,21 @@ public class TossPaymentExecutor implements PaymentExecutor {
                 .isUnknown(false)
                 .isRetryable(false)
                 .build();
+    }
+
+    private static Function<ClientResponse, Mono<? extends Throwable>> getClientResponseError() {
+        return clientResponse ->
+                clientResponse.bodyToMono(TossPaymentConfirmationResponse.TossFailureResponse.class)
+                        .flatMap(tossFailureResponse -> {
+                            TossPaymentError tossPaymentError = TossPaymentError.get(tossFailureResponse.getCode());
+                            return Mono.error(PSPConfirmationException.builder()
+                                    .errorCode(tossPaymentError.name())
+                                    .errorMessage(tossPaymentError.getDescription())
+                                    .isSuccess(tossPaymentError.isSuccess())
+                                    .isFailure(tossPaymentError.isFailure())
+                                    .isUnknown(tossPaymentError.isUnknown())
+                                    .isRetryableError(tossPaymentError.isRetryableError())
+                                    .build());
+                        });
     }
 }
---
+++ module-junkyard-payment-domain/src/main/java/junkyard/payment/infrastructure/payment/toss/exception/PSPConfirmationException.java
@@ -0,0 +1,41 @@
+package junkyard.payment.infrastructure.payment.toss.exception;
+
+import junkyard.payment.domain.order.PaymentOrder;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.stream.Stream;
+
+@Getter
+public class PSPConfirmationException extends Throwable{
+    private final String errorCode;
+    private final String errorMessage;
+    private final Boolean isSuccess;
+    private final Boolean isFailure;
+    private final Boolean isUnknown;
+    private final Boolean isRetryableError;
+    private final Throwable cause;
+
+    @Builder
+    public PSPConfirmationException(String errorCode, String errorMessage, Boolean isSuccess,
+                                    Boolean isFailure, Boolean isUnknown, Boolean isRetryableError, Throwable cause) {
+
+        long count = Stream.of(isSuccess, isFailure, isUnknown).filter(v -> v != null && v).count();
+        if (count != 1) {
+            throw new IllegalArgumentException(String.format("(%s) 는 올바르지 않는 결제 상태입니다.", this.getClass().getName()));
+        }
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+        this.isSuccess = isSuccess;
+        this.isFailure = isFailure;
+        this.isUnknown = isUnknown;
+        this.isRetryableError = isRetryableError;
+        this.cause = cause;
+    }
+
+    public PaymentOrder.PaymentOrderStatus paymentStatus() {
+        if (isSuccess) return PaymentOrder.PaymentOrderStatus.SUCCESS;
+        else if (isFailure) return PaymentOrder.PaymentOrderStatus.FAILURE;
+        else if (isUnknown) return PaymentOrder.PaymentOrderStatus.UNKNOWN;
+        throw new IllegalArgumentException("결제 는 올바르지 않은 결제 상태입니다.");
+    }}
